313fb718efd18c4de524796cb004377c9b9d94f8
quannguyennn <wantmorexx9x@gmail.com>
2019-05-04 14:45:54 +0700

    cloud backend

---
+++ api/auth/router.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Router = express.Router();
+const bcrypt = require("bcryptjs");
+const authRouter = Router;
+
+const userModel = require("../user/model");
+
+authRouter.post("/", (req, res) => {
+    const {account, password} = req.body;
+    if(!account || !password){
+        res.status(400).send({success: 0, message: "Thiếu account hoặc password"});
+    } else {
+        userModel.findOne({account})
+        .then(userFound => {
+            if(!userFound || !userFound._id){
+                res.status(404).send({success: 0, message: "Nguoi dung khong ton tai"});
+            } else {
+                if(bcrypt.compareSync(password, userFound.password)){
+                    req.session.userAccount = {account};
+                    res.status(200).redirect("http://localhost:3000/")
+                } else {
+                    res.status(401).send({success: 0, message: "Sai mat khau"});
+                }
+            }
+        })
+        .catch(err => {
+            res.status(500).send({success: 0, message: err});
+        })
+    }
+})
+
+authRouter.get("/me", (req, res) => {
+    if(!req.session.userAccount){
+        res.status(401).send({success: 0, message: "Ban chua dang nhap"})
+    } else {
+        userModel.findOne(req.session.userAccount, "-password")
+        .then(userFound => {
+            res.send({success: 1, message: userFound});
+        })
+        .catch(err => {
+            res.status(500).send({success: 0, message: err});
+        })
+    }
+})
+
+module.exports = authRouter;
